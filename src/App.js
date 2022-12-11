62e464654a64ef3b0fe13151cc1a01b50c07229c
RUPESH <rupeshpandey27drdo@gmail.com>
2022-12-11 20:31:02 +0530

    My rat in maze project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Problem from "./components/Problem";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h3 className="Heading"> Rat in maze </h3>
+      <Problem />
+    </div>
+  );
+}
+
+export default App;
